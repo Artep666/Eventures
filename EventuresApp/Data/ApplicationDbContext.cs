31f80f460a1f943bc80b6a696be478a4562b5a47
Your Name <artepnikolaeva75@gmail.com>
2023-07-27 21:48:08 +0300

    Add project files.

---
+++ EventuresApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventuresApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+            //password for first registration test@test.com | Test123.
+        }
+    }
+}
