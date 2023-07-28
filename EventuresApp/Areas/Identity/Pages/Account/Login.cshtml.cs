efeb1dd3c4b3a9f6c817c3d4dd7a5158c2f6cc68
Your Name <artepnikolaeva75@gmail.com>
2023-07-28 16:25:05 +0300

    register changes

--- EventuresApp/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ EventuresApp/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -66,8 +66,7 @@ namespace EventuresApp.Areas.Identity.Pages.Account
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Required]
-            [EmailAddress]
-            public string Email { get; set; }
+            public string Username { get; set; }
 
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
