537235575df3da62c60c67f2e383182d439e15dd
Your Name <artepnikolaeva75@gmail.com>
2023-07-29 23:21:37 +0300

    events database

---
+++ EventuresApp/Domain/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EventuresApp.Domain
+{
+    public class Event
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Place { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public int TotalTickets { get; set; }
+
+        [Column(TypeName ="decimal(12,3)")]
+        public decimal PricePerTicket { get; set; }
+
+
+
+
+    }
+}
